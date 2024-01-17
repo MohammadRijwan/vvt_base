ed4612a9aa4a321619bb4aefefc3c082eca41ed4
Mohammad Rijwan Khan <rijwan.kallu@vvtsolutions.in>
2024-01-17 09:20:24 +0530

    fix: added home

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -9,10 +9,37 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset(
-            appAssets?.logo != null ? appAssets!.logo! : Res.logo,
-            package: appAssets?.logo == null ? 'vvt_base' : null),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                  appAssets?.logo != null ? appAssets!.logo! : Res.logo,
+                  package: appAssets?.logo == null ? 'vvt_base' : null),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Home(),
+                      ));
+                },
+                child: const Text('Home')),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home'),
     );
   }
 }
